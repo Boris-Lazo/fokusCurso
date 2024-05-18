03e5f82ed1580363ec3ede709513b39cfb9d4e04
Boris-Lazo <borisstanleylazocastillo@gmail.com>
2024-05-18 12:14:18 -0600

    agregando interactividad al html, por JS

--- script.js
+++ script.js
@@ -3,20 +3,50 @@ const botonCorto = document.querySelector('.app__card-button--corto')
 const botonEnfoque = document.querySelector('.app__card-button--enfoque')
 const botonLargo = document.querySelector('.app__card-button--largo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botones = document.querySelectorAll('.app__card-button')
 
 botonCorto.addEventListener('click', () => {
     cambiarContexto('descanso-corto')
+    botonCorto.classList.add('active')
 })
 
 botonEnfoque.addEventListener('click', () => {
     cambiarContexto('enfoque')
+    botonEnfoque.classList.add('active')
 })
 
 botonLargo.addEventListener('click', () => {
     cambiarContexto('descanso-largo')
+    botonLargo.classList.add('active')
 })
 
 function cambiarContexto(contexto){
+
+    botones.forEach(function(contexto){
+        contexto.classList.remove('active')
+    })
+
     hmtl.setAttribute('data-contexto',contexto)
     banner.setAttribute('src',`./imagenes/${contexto}.png`)
+
+    switch (contexto) {
+        case 'enfoque':
+            titulo.innerHTML = `
+            Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>`
+            break;
+        case 'descanso-corto':
+            titulo.innerHTML = `
+            ¿Qué tal tomar un respiro?,<br> 
+            <strong class="app__title-strong">¡Haz una pausa corta!</strong>`        
+            break;
+        case 'descanso-largo':
+            titulo.innerHTML = `
+            Hora de volver a la superficie.<br>
+            <strong class="app__title-strong">Haz una pausa larga.</strong>`       
+            break;    
+        default:
+            break;
+    }
 }
