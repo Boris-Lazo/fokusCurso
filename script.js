13b0910aa385bcec77adcd2de9d3c690be8a130d
Boris-Lazo <borisstanleylazocastillo@gmail.com>
2024-05-20 21:00:11 -0600

    agrega sonidos y temporizador

--- script.js
+++ script.js
@@ -1,64 +1,102 @@
-const hmtl = document.querySelector('html')
-const botonCorto = document.querySelector('.app__card-button--corto')
-const botonEnfoque = document.querySelector('.app__card-button--enfoque')
-const botonLargo = document.querySelector('.app__card-button--largo')
-const banner = document.querySelector('.app__image')
-const titulo = document.querySelector('.app__title')
-const botones = document.querySelectorAll('.app__card-button')
-const inputEnfoqueMusica = document.querySelector('#alternr-musica')
-const musica = new Audio('./sonido/luna-rise-part-one.mp3')
+const hmtl = document.querySelector('html');
+const botonCorto = document.querySelector('.app__card-button--corto');
+const botonEnfoque = document.querySelector('.app__card-button--enfoque');
+const botonLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+const inputEnfoqueMusica = document.querySelector('#alternar-musica');
+const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+const botonIniciarPausar = document.querySelector('#start-pause');
 
+const sonidoInicio = new Audio('./sonidos/play.wav');
+const sonidoPausa = new Audio('./sonidos/pause.mp3');
+const sonidoFinalizacion = new Audio('./sonidos/beep.mp3');
 
+let tiempoTranscurridoEnSegundos = 5;
+let idIntervalo = null;
 
-inputEnfoqueMusica.addEventListener('change', ()=>{
-    if(musica.paused){
-        musica.play()
-    }else{
-        musica.pause()
+musica.loop = true;
+
+inputEnfoqueMusica.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
     }
-})
+});
 
 botonCorto.addEventListener('click', () => {
-    cambiarContexto('descanso-corto')
-    botonCorto.classList.add('active')
-})
+    cambiarContexto('descanso-corto');
+    botonCorto.classList.add('active');
+});
 
 botonEnfoque.addEventListener('click', () => {
-    cambiarContexto('enfoque')
-    botonEnfoque.classList.add('active')
-})
+    cambiarContexto('enfoque');
+    botonEnfoque.classList.add('active');
+});
 
 botonLargo.addEventListener('click', () => {
-    cambiarContexto('descanso-largo')
-    botonLargo.classList.add('active')
-})
+    cambiarContexto('descanso-largo');
+    botonLargo.classList.add('active');
+});
 
-function cambiarContexto(contexto){
+function cambiarContexto(contexto) {
+    botones.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    });
 
-    botones.forEach(function(contexto){
-        contexto.classList.remove('active')
-    })
-
-    hmtl.setAttribute('data-contexto',contexto)
-    banner.setAttribute('src',`./imagenes/${contexto}.png`)
+    hmtl.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `./imagenes/${contexto}.png`);
 
     switch (contexto) {
         case 'enfoque':
             titulo.innerHTML = `
             Optimiza tu productividad,<br>
-                <strong class="app__title-strong">sumérgete en lo que importa.</strong>`
+                <strong class="app__title-strong">sumérgete en lo que importa.</strong>`;
             break;
         case 'descanso-corto':
             titulo.innerHTML = `
             ¿Qué tal tomar un respiro?,<br> 
-            <strong class="app__title-strong">¡Haz una pausa corta!</strong>`        
+            <strong class="app__title-strong">¡Haz una pausa corta!</strong>`;
             break;
         case 'descanso-largo':
             titulo.innerHTML = `
             Hora de volver a la superficie.<br>
-            <strong class="app__title-strong">Haz una pausa larga.</strong>`       
-            break;    
+            <strong class="app__title-strong">Haz una pausa larga.</strong>`;
+            break;
         default:
             break;
     }
+}
+
+const cuentaRegresiva = () => {
+    if (tiempoTranscurridoEnSegundos <= 0) {
+        reiniciar();
+        sonidoFinalizacion.play();
+        alert('Tiempo final');
+        return;
+    }
+    if (tiempoTranscurridoEnSegundos === 0) { // Suena cuando faltan 0 segundos
+        sonidoFinalizacion.play();
+    }
+    tiempoTranscurridoEnSegundos -= 1;
+    console.log('Temporizador:' + tiempoTranscurridoEnSegundos);
+};
+
+botonIniciarPausar.addEventListener('click', iniciarPausar);
+
+function iniciarPausar() {
+    if (idIntervalo) {
+        reiniciar();
+        sonidoPausa.play();
+        return;
+    }
+    sonidoInicio.play();
+    idIntervalo = setInterval(cuentaRegresiva, 1000);
+}
+
+function reiniciar() {
+    clearInterval(idIntervalo);
+    idIntervalo = null;
 }
